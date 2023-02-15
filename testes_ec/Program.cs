4e39f7105b3929dd84994e4620d346267da58732
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2023-02-15 20:17:51 -0300

    #4

---
+++ testes_ec/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testes_ec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 5;
+
+            Console.WriteLine("Bom dia");
+
+            if (x < 0)
+            {
+                Console.WriteLine("Boa tarde!");
+
+            }
+                Console.WriteLine("Boa noite");
+
+
+                Console.ReadLine();
+
+            }
+        }
+
+        }
+    
+
