ea48aa5a19e01ce5153fc883bb9e875b0f8ada39
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2023-02-18 17:55:15 -0300

    #9

---
+++ DoWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    class Program
+    {
+  
+        static void Main(string[] args){
+
+            double C, F;
+
+
+            Console.Write("Digite a temperatura em celsius: ");
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            F = 9.0 * C/ 5.0 + 32.0;
+            Console.WriteLine("Equivalente em Fahrenheit: " + F.ToString("F1", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+                }
+            }
+        }
+    
+
